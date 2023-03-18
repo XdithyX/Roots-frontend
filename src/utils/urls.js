405f9154b1ce5e7235d5d90452c034c07f74678f
Adithya Ajith <4adhi007@gmail.com>
2023-03-18 22:20:19 +0530

    m

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
 // export const baseUrl='https://roots-api-production.up.railway.app' 
 //production
-export const baseUrl='http://localhost:8000'       
+export const baseUrl='https://techno-backend-production.up.railway.app'       
              
 //local
