a33849a107418c1159088c2aed0764d955a3dee7
jacobsanosh <jacobsanosh@gmail.com>
2023-03-19 01:32:29 +0530

    adding

--- src/pages/Review/Reviewcards.js
+++ src/pages/Review/Reviewcards.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import MainLayout from '../../components/MainLayout/MainLayout'
-import './Reviewcards.scss'
+import React from "react";
+import MainLayout from "../../components/MainLayout/MainLayout";
+import "./Reviewcards.scss";
+import img from "../../../src/assets/png/guide.png";
 function Reviewcards() {
   return (
-   <MainLayout>
-   <div className='reviewpg'>
-   <div className='reviewpg__heading'>
-  <h1> Tourist Experiences</h1>
-   </div>
-    <div class="review">
-       <div class="reviewer">
-     <h3>John Doe</h3>
-     </div> *
-   <div class="review-content">
-     <h4>Amazing Experience</h4>
-     <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id purus quis lectus sollicitudin congue. Morbi porta quam id risus dignissim consectetur. Nullam luctus metus nec erat commodo, non facilisis velit ultricies. </p>
-    
-   </div>
- </div>
- </div>
-
-   </MainLayout>
-  )
+    <MainLayout>
+      <div className="reviewpg">
+        <div className="reviewpg__heading">
+          <h1> Tourist Experiences</h1>
+        </div>
+        <div class="review__div">
+          <div className="rev__user__img">
+            <img src={img} className="rev__user__img" alt="" />
+          </div>
+          <div className="rev__details">
+            <p>user__name</p>
+            <p>place</p>
+            <p>user revies goes here</p>
+            <div className="user__rev__star">
+            <p>stars goes here</p>
+          </div>
+          </div>
+          
+        </div>
+      </div>
+    </MainLayout>
+  );
 }
 
-export default Reviewcards
+export default Reviewcards;
