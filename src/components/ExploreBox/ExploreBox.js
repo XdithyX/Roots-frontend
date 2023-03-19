7d9d6d47c3bb4a6b9d9e39aa65683063de56a4a3
Adithya Ajith <4adhi007@gmail.com>
2023-03-19 06:36:31 +0530

    added evnt cultural

--- src/components/ExploreBox/ExploreBox.js
+++ src/components/ExploreBox/ExploreBox.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import './ExploreBox.css'
 import img from '../../../src/assets/png/guide.png'
+import {CiLocationOn} from 'react-icons/ci'
 function ExploreBox({eventimg,eventname,eventdesc,eventloc}) {
   return (
     <div className='expbox'>
        <div className='exp__img'>
-        <img src={img} alt='ento' className='exp__img'></img>
+        <img src={eventimg} alt='ento' className='exp__img'></img>
        </div>
        <div className='exp__content'>
-        <h3></h3>
-        <p>place</p>
-        <p>desc</p>
+        <h3 className='cul__eventname'>{eventname}</h3>
+        <div className='cul__loc'>
+        <CiLocationOn/>
+        <p style={{ color:'#fff' }}>{eventloc}</p>
+        </div>
+        <p style={{ color:'#fff' , textAlign:'left' }}>{eventdesc}</p>
         <button className='exp__btn'>Book Tickets</button>
        </div>
     </div>
